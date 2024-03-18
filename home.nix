1cdeacfd532b4caa9788efbd74c25e3d5e74cbd8
chell4 <arsenijalfeev@gmail.com>
2024-03-18 08:47:17 +0300

    add nushell to config

--- home.nix
+++ home.nix
@@ -62,6 +62,20 @@
       enable = true;
     };
 
+    nushell = {
+      enable = true;
+    };
+
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
+
+    starship = {
+      enable = true;
+      settings.add_newline = true;
+    };
+
     tmux = {
       enable = true;
       extraConfig = ''

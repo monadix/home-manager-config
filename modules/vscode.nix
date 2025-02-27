886a8ac41a7bd5a6722cfbc42b06375a566aab7f
chell4 <arsenijalfeev@gmail.com>
2025-02-27 14:49:54 +0300

    update: flake

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -5,47 +5,51 @@
 {
   programs.vscode = rec {
     enable = true;
-    enableUpdateCheck = false;
-    enableExtensionUpdateCheck = false;
-
     package = pkgs.vscode-fhs;
 
-    userSettings ={
-      "editor.lineNumbers" = "relative";
-      "extensions.autoCheckUpdates" = false;
-      "update.mode" = "none";
-      "extensions.experimental.affinity" = {
-        "asvetliakov.vscode-neovim" = 1;
+    profiles.default = {
+
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+
+
+      userSettings ={
+        "editor.lineNumbers" = "relative";
+        "extensions.autoCheckUpdates" = false;
+        "update.mode" = "none";
+        "extensions.experimental.affinity" = {
+          "asvetliakov.vscode-neovim" = 1;
+        };
+        "git.openRepositoryInParentFolders" = "always";
+        "workbench.colorTheme" = "Nord";
+        "codeium.enableConfig" = {
+          "*" = true;
+          "nix" = true;
+          "properties" = true;
+          "proto" = true;
+        };
       };
-      "git.openRepositoryInParentFolders" = "always";
-      "workbench.colorTheme" = "Nord";
-      "codeium.enableConfig" = {
-        "*" = true;
-        "nix" = true;
-        "properties" = true;
-        "proto" = true;
-      };
+
+      extensions = with vscode-extensions.vscode-marketplace; [
+        tamasfe.even-better-toml
+        golang.go
+
+        haskell.haskell
+        justusadam.language-haskell
+        ms-vscode.makefile-tools
+        bbenoist.nix
+        arrterian.nix-env-selector
+        arcticicestudio.nord-visual-studio-code
+        peterj.proto
+        rust-lang.rust-analyzer
+        ms-vscode-remote.remote-containers
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        ms-python.python
+        asvetliakov.vscode-neovim
+        thenuprojectcontributors.vscode-nushell-lang
+        rangav.vscode-thunder-client
+      ];
     };
-
-    extensions = with vscode-extensions.vscode-marketplace; [
-      tamasfe.even-better-toml
-      golang.go
-
-      haskell.haskell
-      justusadam.language-haskell
-      ms-vscode.makefile-tools
-      bbenoist.nix
-      arrterian.nix-env-selector
-      arcticicestudio.nord-visual-studio-code
-      peterj.proto
-      rust-lang.rust-analyzer
-      ms-vscode-remote.remote-containers
-      ms-vscode-remote.remote-ssh
-      ms-vscode-remote.remote-ssh-edit
-      ms-python.python
-      asvetliakov.vscode-neovim
-      thenuprojectcontributors.vscode-nushell-lang
-      rangav.vscode-thunder-client
-    ];
   };
 }

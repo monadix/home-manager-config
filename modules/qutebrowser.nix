e89e1978eaa0a98c0096ea12912da6aa82c9a0ed
monadix <arsenijalfeev@gmail.com>
2025-05-07 09:09:28 +0300

    feat: dark colorscheme and downloads path for qutebrowser

--- modules/qutebrowser.nix
+++ modules/qutebrowser.nix
@@ -4,5 +4,14 @@
 {
   programs.qutebrowser = {
     enable = true;
+
+    settings = {
+      colors.webpage.preferred_color_scheme = "dark";
+
+      downloads.location = {
+        directory = "~/Downloads/qute/";
+        prompt = false;
+      };
+    };
   };
 }

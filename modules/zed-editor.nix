ef61be581bb6281b104e3d2a15ee92779e4affd0
monadix <arsenijalfeev@gmail.com>
2025-05-31 10:47:20 +0300

    feat(zed): some rust-analyzer config

--- modules/zed-editor.nix
+++ modules/zed-editor.nix
@@ -8,23 +8,45 @@
     installRemoteServer = true;
 
     extensions = [
+      "git-firefly"
       "nix"
       "nord"
+      "toml"
     ];
 
     extraPackages = with pkgs; [
-      rust-analyzer
+      nil
       nixd
+      rust-analyzer
+      vscode-langservers-extracted
+      package-version-server
     ];
 
     userSettings = {
       ui_font_size = 16;
       buffer_font_size = 16;
 
+      theme = "Nord";
+
       vim_mode = true;
       relative_line_numbers = true;
 
-      theme = "Nord";
+      edit_predictions.mode = "subtle";
+
+      inlay_hints = {
+        enabled = true;
+      };
+
+      lsp.rust_analyzer.initialization_options.inlayHints = {
+        maxLength = null;        
+        lifetimeEllistionHints = {
+          enable = "skip_trivial";
+          useParameterNames = true;
+        };
+        closureReturnTypeHints = {
+          enable = "always";
+        };
+      };
     };
   };
 }

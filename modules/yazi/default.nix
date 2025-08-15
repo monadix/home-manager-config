0dfd7804814a1a50e2dc3d8640d8e12da1b300c6
monadix <arsenijalfeev@gmail.com>
2025-08-15 16:07:37 +0300

    feat(yazi): add keybind for dropping to shell

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -2,7 +2,16 @@
 {
   programs.yazi = {
     enable = true;
-
     enableNushellIntegration = true;
+
+    keymap = {
+      mgr.prepend_keymap = [
+        { 
+          run  = ''shell "$SHELL" --block'';
+          on   = [ "!" ];
+          desc = "Open $SHELL here";
+        }
+      ];
+    };
   };
 }

3736fc60c9b580ccb44bbc7bbe1ccffea9c141ed
monadix <arsenijalfeev@gmail.com>
2026-02-25 16:05:48 +0300

    feat(opencode): add context7 mcp server

--- modules/opencode.nix
+++ modules/opencode.nix
@@ -8,7 +8,9 @@
     enable = true;
   };
 
-  sops.templates."opencode.json".content = ''{
+  sops.templates."opencode.json".content = let
+    placeholders = config.sops.placeholder;
+  in ''{
     "$schema": "https://opencode.ai/config.json",
 
     "theme": "nord",
@@ -18,7 +20,7 @@
         "npm": "@ai-sdk/openai-compatible",
         "name": "Ollama",
         "options": {
-          "baseURL": "${config.sops.placeholder.internal-ollama-url}"
+          "baseURL": "${placeholders.internal-ollama-url}"
         },
         "models": {
           "qwen3-coder-next:q8_0": {
@@ -26,22 +28,47 @@
           }
         }
       }
+    },
+
+    "mcp": {
+      "context7": {
+        "type": "remote",
+        "url": "https://mcp.context7.com/mcp",
+        "headers": {
+          "CONTEXT7_API_KEY": "${placeholders.context7-api-key}"
+        },
+        "enabled": true
+      }
     }
   }'';
 
-  sops.secrets.internal-ollama-url = {};
+  sops.secrets = {
+    internal-ollama-url = {};
+    context7-api-key = {};
+  };
 
   systemd.user.services.opencode-write-config = {
     Unit = {
-      X-SwitchMethod = "restart";
       Description = "write opencode config";
+
+      After = [ "sops-nix.service" ];
+
+      X-SwitchMethod = "restart";
+      X-Reload-Triggers = [
+        (builtins.hashString "md5" config.sops.templates."opencode.json".content)
+      ];
     };
+
     Service = {
       ExecStart = "${pkgs.writers.writeBash "opencode-write-config" ''
-        ${pkgs.coreutils-full}/bin/rm -f ~/.config/opencode/opencode.json
-        ${pkgs.coreutils-full}/bin/mkdir -p ~/.config/opencode 
-        ${pkgs.coreutils-full}/bin/cp ${config.sops.templates."opencode.json".path} ~/.config/opencode/opencode.json 
+        ${pkgs.coreutils-full}/bin/install -m 400 -D ${config.sops.templates."opencode.json".path} ~/.config/opencode/opencode.json 
       ''}";
+    };
+
+    Install = {
+      WantedBy = [
+        "default.target"
+      ];
     };
   };
 }
